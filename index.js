0c3ab35dc724dfa4c6f3f290a85a35fd5e18d56d
Alvin Ooi <alvinooizy@gmail.com>
2026-06-26 10:09:06 +0800

    fix port for render

--- index.js
+++ index.js
@@ -351,6 +351,8 @@ app.get("/product/:id", (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
